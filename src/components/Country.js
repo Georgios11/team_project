6eaa12cab9c7e6e1edecac9e158c7b1c366ac52c
Georgios11 <tsompanidis.georgios@gmail.com>
2023-02-26 09:40:15 +0100

    Georgios done

--- src/components/Country.js
+++ src/components/Country.js
@@ -2,16 +2,20 @@ import React from 'react';
 
 const Country = ({ country }) => {
     return (
-        <article className="country ">
+        <article className="card">
             <figure>
                 <img src={country.flags.svg} alt="" className="flag " />
                 <figcaption className="caption">
                     <h2 className="country__name">{country.name.common}</h2>
-                    <h3 className="country__name">{!country.capital ? `No capital` : country.capital}</h3>
-                    <p className="country__name">{country.population}</p>
-                    <button>Delete Country</button>
                 </figcaption>
             </figure>
+            <div class="card-content">
+                <h2>Capital</h2>
+                <h3 className="country__name">{!country.capital ? `No capital` : country.capital}</h3>
+                <h4>Population</h4>
+                <p className="country__population">{country.population}</p>
+            </div>
+            <button>Delete Country</button>
         </article>
     );
 };
