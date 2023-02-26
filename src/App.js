c251e2de90090f33b5902aae0b5d844e9b038bf8
theRomeo505 <ivaroma2001@gmail.com>
2023-02-26 17:38:31 +0200

    search&sort

--- src/App.js
+++ src/App.js
@@ -12,30 +12,46 @@ const API_URL = `${countryApi}/v3.1/all`;
 
 function App() {
     const { data, isLoading, error } = useFetch(API_URL);
-    const [countriesState, dispatch] = useReducer(reducer, { countries: data, error: null });
+    const [countriesState, dispatch] = useReducer(reducer, { countries: data, displayed: data, error: null, isDescending: true });
+    ///// how to do it properly ???
     useEffect(() => {
-        console.log('changge', data.length);
         if (data.length !== 0) {
             dispatch({ type: 'SET', payload: { countries: data } });
         }
         //countriesState.countries = data;
     }, [isLoading]);
+    const handleSearch = (event) => {
+        const input = event.target.value;
 
-    //    dispatch({type:"ADD", payload: country });
-    //    dispatch({type:"DELETE",payload: id})
+        const search = (country) => {
+            return country.name.common.toLowerCase().includes(input.toLowerCase()) || (country.capital && country.capital[0].toLowerCase().includes(input.toLowerCase()));
+        };
 
-    // countriesState.countries
-    // name population capital flag continent
-    console.log('data', data);
-    console.log('islo', isLoading);
-    console.log('count', countriesState);
+        dispatch({ type: 'SEARCH', payload: { displayed: countriesState.countries.filter(search) } });
+    };
+    const handleSort = () => {
+        countriesState.displayed.sort((x, y) => {
+            if (x.name.common.localeCompare(y.name.common) === 1) return countriesState.isDescending ? 1 : -1;
+            if (x.name.common.localeCompare(y.name.common) === -1) return countriesState.isDescending ? -1 : 1;
+        });
+
+        dispatch({
+            type: 'SORT',
+            payload: {
+                displayed: countriesState.displayed
+            }
+        });
+    };
     return (
         <div>
             <Header />
             <main className="main">
+                <input type={'text'} name={'search'} placeholder={'Search'} onChange={handleSearch} id="search"></input>
+                <button name="sort" id="sort" onClick={handleSort}>
+                    {'Sort ' + (countriesState.isDescending ? '⇩' : '⇧')}
+                </button>
                 {error && <h2>{error}</h2>}
-                {isLoading && countriesState.countries ? <span className="loading">Loading...</span> : <Countries countries={countriesState.countries} />}
-                {/* {data.length > 0 && console.log(data.slice(0, 10))} */}
+                {isLoading && countriesState.displayed ? <span className="loading">Loading...</span> : <Countries countries={countriesState.displayed} />}
             </main>
             <Footer />
         </div>
--- src/utils/reducer.js
+++ src/utils/reducer.js
@@ -1,14 +1,17 @@
 const reducer = (state, action) => {
     switch (action.type) {
         case 'SET':
-            console.log('set');
-            return { ...state, countries: action.payload.countries };
+            return { ...state, countries: action.payload.countries, displayed: action.payload.countries };
         case 'ADD':
             const country = action.payload.country;
             return { ...state, countries: [...state.countries, country] };
         case 'DELETE':
             const id = action.payload.id;
             return { ...state, countries: [...state.filter((e) => e.id !== id)] };
+        case 'SEARCH':
+            return { ...state, displayed: action.payload.displayed };
+        case 'SORT':
+            return { ...state, displayed: action.payload.displayed, isDescending: !state.isDescending };
         case 'UPDATE':
             // update to be done
             return 3;
